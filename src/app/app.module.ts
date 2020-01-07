ec0b34f28302a94afb9109b425657738717a58a3
Patrick Talom <patrycktalom@gmail.com>
2020-01-07 10:36:49 +0300

    Home Update
    Taf sur le Home principalement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    MenuComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
